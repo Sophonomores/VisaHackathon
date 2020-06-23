5e515f1aea19d8a814f4b094b00acc5995682be9
Ni Tianzhen <nitianzhen1210@gmail.com>
2020-06-23 20:59:48 +0800

    Set up restful api

--- app/src/main/java/com/sophonomores/restaurantorderapp/services/Advertiser.java
+++ app/src/main/java/com/sophonomores/restaurantorderapp/services/Advertiser.java
@@ -4,24 +4,38 @@ import android.content.Context;
 
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
+import com.sophonomores.restaurantorderapp.services.api.ApiEndpoint;
+import com.sophonomores.restaurantorderapp.services.api.ApiException;
+import com.sophonomores.restaurantorderapp.services.api.Parser;
+import com.sophonomores.restaurantorderapp.services.api.Request;
+
+import androidx.annotation.NonNull;
 
 public class Advertiser {
 
-    private final Context mContext;
+    private final Context context;
     private final String DEVICE_NAME = "MERCHANT_APP"; // hardcoded for now
     private final String SERVICE_ID = "com.sophonomores.restaurantorderapp"; // hardcoded for now
-    private final ConnectionLifecycleCallback connectionLifecycleCallback = new ConnectionCallback();
+    private final ConnectionLifecycleCallback connectionLifecycleCallback = new MerchantConnectionCallback();
+    private final PayloadCallback payloadCallback = new MerchantPayloadCallback();
 
     public Advertiser(Context context) {
-        mContext = context;
+        this.context = context;
     }
 
     public void startAdvertising() {
+        System.out.println(ConnectionsClient.MAX_BYTES_DATA_SIZE);
         AdvertisingOptions advertisingOptions =
                 new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
-        Nearby.getConnectionsClient(mContext)
+        Nearby.getConnectionsClient(context)
                 .startAdvertising(
                         DEVICE_NAME, SERVICE_ID, connectionLifecycleCallback, advertisingOptions)
                 .addOnSuccessListener(
@@ -32,5 +46,47 @@ public class Advertiser {
                         (Exception e) -> {
                             System.err.println("We were unable to start advertising: " + e.getMessage());
                         });
+    }
+
+    private class MerchantConnectionCallback extends ConnectionLifecycleCallback {
+        @Override
+        public void onConnectionInitiated(@NonNull String endpointId, @NonNull ConnectionInfo connectionInfo) {
+            String message = String.format("%s: Connection initiated: %s", endpointId, connectionInfo.toString());
+            System.out.println(message);
+            Nearby.getConnectionsClient(context).acceptConnection(endpointId, payloadCallback);
+        }
+
+        @Override
+        public void onConnectionResult(@NonNull String endpointId, @NonNull ConnectionResolution connectionResolution) {
+            String message = String.format("%s: Connection result: %s", endpointId, connectionResolution.toString());
+            System.out.println(message);
+        }
+
+        @Override
+        public void onDisconnected(@NonNull String endpointId) {
+            System.out.println(endpointId + ": Disconnected.");
+        }
+    }
+
+    private class MerchantPayloadCallback extends PayloadCallback {
+        @Override
+        public void onPayloadReceived(@NonNull String endpointId, @NonNull Payload payload) {
+            String message = String.format("%s: Payload received in bytes: %s", endpointId, new String(payload.asBytes()));
+            System.out.println(message);
+            Request request = Parser.parseRequest(new String(payload.asBytes()));
+            String response;
+            try {
+                response = ApiEndpoint.getAction(request.getUri(), request.getMethod()).execute();
+            } catch (ApiException e) {
+                response = e.getStatusCode();
+            }
+            Payload bytesPayload = Payload.fromBytes(response.getBytes());
+            Nearby.getConnectionsClient(context).sendPayload(endpointId, bytesPayload);
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(@NonNull String endpointId, @NonNull PayloadTransferUpdate payloadTransferUpdate) {
+
+        }
     }
 }
--- app/src/main/java/com/sophonomores/restaurantorderapp/services/Discoverer.java
+++ app/src/main/java/com/sophonomores/restaurantorderapp/services/Discoverer.java
@@ -3,25 +3,43 @@ package com.sophonomores.restaurantorderapp.services;
 import android.content.Context;
 
 import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Strategy;
+import com.sophonomores.restaurantorderapp.services.api.ApiEndpoint;
+import com.sophonomores.restaurantorderapp.services.api.ResourceURIs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 
 public class Discoverer {
 
-    private final Context mContext;
+    private final Context context;
     private final String DEVICE_NAME = "CLIENT_APP"; // hardcoded for now
     private final String SERVICE_ID = "com.sophonomores.restaurantorderapp"; // hardcoded for now
     private final EndpointDiscoveryCallback clientDiscoveryCallback = new ClientDiscoveryCallback();
 
+    private final List<String> devices;
+
     public Discoverer(Context context) {
-        mContext = context;
+        this.context = context;
+        devices = new ArrayList<>();
+    }
+
+    public List<String> getDevices() {
+        List<String> result = new ArrayList<>();
+        Collections.copy(result, devices);
+        return result;
     }
 
     public void startDiscovery() {
         DiscoveryOptions discoveryOptions =
                 new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
-        Nearby.getConnectionsClient(mContext)
+        Nearby.getConnectionsClient(context)
                 .startDiscovery(SERVICE_ID, clientDiscoveryCallback, discoveryOptions)
                 .addOnSuccessListener(
                         (Void unused) -> {
@@ -31,5 +49,24 @@ public class Discoverer {
                         (Exception e) -> {
                             System.err.println("We were unable to start discovering: " + e.getMessage());
                         });
+    }
+
+    private class ClientDiscoveryCallback extends EndpointDiscoveryCallback {
+        @Override
+        public void onEndpointFound(@NonNull String endpointId, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
+            String message = String.format("%s: Endpoint found: %s", endpointId, discoveredEndpointInfo.toString());
+            System.out.println(message);
+            if (!devices.contains(endpointId)) {
+                devices.add(endpointId);
+                // Use this to make API calls in the app:
+                new Messenger(context, DEVICE_NAME).get(endpointId, ResourceURIs.MENU, System.out::println);
+            }
+        }
+
+        @Override
+        public void onEndpointLost(@NonNull String endpointId) {
+            System.out.println(endpointId + ": Endpoint lost.");
+            devices.remove(endpointId);
+        }
     }
 }
