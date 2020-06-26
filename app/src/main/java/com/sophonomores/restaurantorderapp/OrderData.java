9618462a6b621ba81813bebb27d0361a44622436
Chen Su <chensu@comp.nus.edu.sg>
2020-06-26 14:46:24 +0800

    Integrate connection api with merchant side. 1. Refactored DataSource class into RestaurantData and OrderData classes 2. Set up notifier and listen at merchant side 3. Update PostCheckoutAction class.

--- app/src/main/java/com/sophonomores/restaurantorderapp/OrderData.java
+++ app/src/main/java/com/sophonomores/restaurantorderapp/OrderData.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class OrderData {
 
     private Context context;
-    private OrderListener listener;
+    private static OrderListener listener;
 
     public OrderData(Context context) {
         this.context = context;
@@ -25,70 +25,32 @@ public class OrderData {
         this.listener = listener;
     }
 
-    public void notifyListenerToAddOrder(Order order) {
+    public static void notifyListenerToAddOrder(Order order) {
         listener.onNewOrder(order);
         System.out.println("New order coming from: " +  order.getCustomerName());
     }
 
-//    public void getListOfNearbyOrders() {
+//    public static List<Order> getConfirmedOrder () {
 //
-//        Discoverer discoverer = new Discoverer(context);
-//        discoverer.startDiscovery();
-//        List<String> endpointIds = discoverer.getDevices();
+//        Restaurant steakHouse =  RestaurantData.makeSteakHouse();
+//        List<Dish> western_one = new ArrayList<>();
+//        western_one.add(new Dish("Sirloin", 12.50));
+//        Order order_one = Order.confirmOrder(new UserProfile("Alice"), steakHouse, western_one);
 //
-//        // hardcoded device ids
-//        endpointIds = Arrays.asList("a", "b", "c");
-//        System.out.println("end points found: " + endpointIds);
-//        // hardcoded list of restaurants
-//        List<Restaurant> restaurantsFound = getRestaurantData();
+//        List<Dish> western_two = new ArrayList<>();
+//        western_two.add(new Dish("Rib eye", 13.50));
+//        western_two.add(new Dish("Angus Beef", 14.50));
+//        Order order_two = Order.confirmOrder(new UserProfile("Bob"), steakHouse, western_two);
 //
-//        for (String endpointId : endpointIds) {
-//            Messenger messenger = new Messenger(context, discoverer.DEVICE_NAME);
-//            // TODO: uncomment this when ready
-////            messenger.get(endpointId, ResourceURIs.MENU, (String response) -> {
-////                Gson gson = new Gson();
-////                Restaurant restaurant = gson.fromJson(response, Restaurant.class);
-////                notifyListenerToAddRestaurant(restaurant);
-////            });
 //
-//            // hardcoded response
-//            notifyListenerToAddRestaurant(restaurantsFound.get(endpointIds.indexOf(endpointId)));
-//        }
+//        List<Order> orders = new ArrayList<>();
+//        orders.add(order_one);
+//        orders.add(order_two);
+//
+//        return orders;
 //    }
 
-
-//    // TODO: replace hard-coded order info
-//    public static Restaurant makeSteakHouse() {
-//        List<Dish> westernDishes = new ArrayList<>();
-//        westernDishes.add(new Dish("Sirloin", 12.50));
-//        westernDishes.add(new Dish("Rib eye", 13.50));
-//        westernDishes.add(new Dish("Angus Beef", 14.50));
-//
-//        return new Restaurant("Steak House", "Western", westernDishes);
-//    }
-
-
-    public static List<Order> getConfirmedOrder () {
-
-        Restaurant steakHouse =  new Restaurant("Steak House", "Western");
-        List<Dish> western_one = new ArrayList<>();
-        western_one.add(new Dish("Sirloin", 12.50));
-        Order order_one = Order.confirmOrder(new UserProfile("Alice"), steakHouse, western_one);
-
-        List<Dish> western_two = new ArrayList<>();
-        western_two.add(new Dish("Rib eye", 13.50));
-        western_two.add(new Dish("Angus Beef", 14.50));
-        Order order_two = Order.confirmOrder(new UserProfile("Bob"), steakHouse, western_two);
-
-
-        List<Order> orders = new ArrayList<>();
-        orders.add(order_one);
-        orders.add(order_two);
-
-        return orders;
-    }
-
-    // Classes that want to observe changes in the list of restaurants discovered
+    // Classes that want to observe changes in the list of orders received
     // should implement this interface to get notified.
     public interface OrderListener {
         void onNewOrder(Order order);
